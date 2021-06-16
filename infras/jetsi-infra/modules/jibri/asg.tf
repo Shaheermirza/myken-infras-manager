2af971f4d38e4de9f411c9cb01d1433c7079ab78
yoletech-consulting <yoletech.consulting@gmail.com>
2021-06-16 15:34:29 +0100

    add jetsi.jibri module

--- infras/jetsi-infra/modules/jibri/asg.tf
+++ infras/jetsi-infra/modules/jibri/asg.tf
@@ -60,7 +60,7 @@ resource "aws_launch_configuration" "jibri" {
     associate_public_ip_address      = false
     ebs_optimized                    = false
     enable_monitoring                = false
-    iam_instance_profile             = "arn:aws:iam::988339190536:instance-profile/ASG_JIBRI_ROLE"
+    #iam_instance_profile             = "arn:aws:iam::988339190536:instance-profile/ASG_JIBRI_ROLE"
     image_id                         = "ami-01af747a82cdd687c"
     instance_type                    = "t2.xlarge"
     key_name                         = "meet"
